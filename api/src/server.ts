830beafc90654ccd20afa24007ef4a3c976546b0
Luis V. Capelletto <thecapellett@gmail.com>
2022-10-05 19:48:59 -0300

    ✨ config koa router

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,22 @@
+import Router from '@koa/router'
 import Koa from 'koa'
-const app = new Koa()
 
-app.use(async (ctx) => {
-  ctx.body = 'Hello World'
+const app = new Koa()
+const router = new Router()
+
+router.get('/', async (ctx) => {
+  ctx.body = {
+    message: 'Hello World!',
+  }
 })
+
+router.get('/users', async (ctx) => {
+  ctx.body = {
+    message: 'Users!',
+  }
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 app.listen(3333, () => console.log('Server running on port 3333'))
