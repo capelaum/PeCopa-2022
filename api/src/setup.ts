7949c8b2829fddcb7cec2ee8e3658c8d25ad0835
Luis V. Capelletto <thecapellett@gmail.com>
2022-10-05 20:02:03 -0300

    add packages

--- api/src/setup.ts
+++ api/src/setup.ts
@@ -1,7 +1,11 @@
+import cors from '@koa/cors'
 import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
 import { router } from './router'
 
 export const app = new Koa()
 
+app.use(cors())
+app.use(bodyParser())
 app.use(router.routes())
 app.use(router.allowedMethods())
