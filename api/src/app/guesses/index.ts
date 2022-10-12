4093c2dbfcd706550c6747044e80e3b078fd3165
Luis V. Capelletto <thecapellett@gmail.com>
2022-10-12 00:07:30 -0300

    ♻️ refactor Verify Token

--- api/src/app/guesses/index.ts
+++ api/src/app/guesses/index.ts
@@ -4,72 +4,48 @@ import { NewGuess } from '../../@types/guesses'
 import { prisma } from '../../database/prismaClient'
 
 export const create = async (ctx: RouterContext) => {
-  if (!ctx.headers.authorization) {
-    ctx.status = 401
-    ctx.body = {
-      message: 'Não autorizado.',
-    }
+  const { matchId, homeTeamScore, awayTeamScore } = ctx.request.body as NewGuess
+
+  if (!validateCreateGuessRequest(ctx)) {
+    return
+  }
+
+  const userId = verifyToken(ctx)
+
+  if (!userId) {
     return
   }
 
   try {
-    const [type, accessToken] = ctx.headers.authorization.split(' ')
-    const decodedToken = jwt.verify(accessToken, process.env.JWT_SECRET!)
+    const guessExists = await prisma.guess.findFirst({
+      where: {
+        user_id: userId,
+        match_id: matchId,
+      },
+    })
 
-    if (!decodedToken || !decodedToken.sub) {
-      ctx.status = 401
+    if (guessExists) {
+      ctx.status = 400
       ctx.body = {
-        message: 'Não autorizado.',
+        message: 'Palpite já existe.',
       }
       return
     }
 
-    const userId = decodedToken.sub.toString()
+    const guess = await prisma.guess.create({
+      data: {
+        match_id: matchId,
+        user_id: userId,
+        home_team_score: homeTeamScore,
+        away_team_score: awayTeamScore,
+      },
+    })
 
-    const { matchId, homeTeamScore, awayTeamScore } = ctx.request
-      .body as NewGuess
-
-    if (!validateCreateGuessRequest(ctx)) {
-      return
-    }
-
-    try {
-      const guessExists = await prisma.guess.findFirst({
-        where: {
-          user_id: userId,
-          match_id: matchId,
-        },
-      })
-
-      if (guessExists) {
-        ctx.status = 400
-        ctx.body = {
-          message: 'Palpite já existe.',
-        }
-        return
-      }
-
-      const guess = await prisma.guess.create({
-        data: {
-          match_id: matchId,
-          user_id: userId,
-          home_team_score: homeTeamScore,
-          away_team_score: awayTeamScore,
-        },
-      })
-
-      ctx.status = 201
-      ctx.body = guess
-    } catch (error) {
-      ctx.status = 500
-      ctx.body = { message: (error as Error).message }
-    }
+    ctx.status = 201
+    ctx.body = guess
   } catch (error) {
-    ctx.status = 401
-    ctx.body = {
-      message: 'Não autorizado.',
-    }
-    return
+    ctx.status = 500
+    ctx.body = { message: (error as Error).message }
   }
 }
 
@@ -106,4 +82,37 @@ const validateCreateGuessRequest = (ctx: RouterContext) => {
   }
 
   return true
+}
+
+const verifyToken = (ctx: RouterContext) => {
+  if (!ctx.headers.authorization) {
+    ctx.status = 401
+    ctx.body = {
+      message: 'Não autorizado.',
+    }
+    return false
+  }
+
+  try {
+    const [type, accessToken] = ctx.headers.authorization.split(' ')
+    const decodedToken = jwt.verify(accessToken, process.env.JWT_SECRET!)
+
+    if (!decodedToken || !decodedToken.sub) {
+      ctx.status = 401
+      ctx.body = {
+        message: 'Não autorizado.',
+      }
+      return
+    }
+
+    const userId = decodedToken.sub.toString()
+
+    return userId
+  } catch (error) {
+    ctx.status = 401
+    ctx.body = {
+      message: 'Não autorizado.',
+    }
+    return false
+  }
 }
