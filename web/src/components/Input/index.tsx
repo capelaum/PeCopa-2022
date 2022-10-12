d6f764de88b8751dd376f5fee095a06d5f401a25
Luis V. Capelletto <thecapellett@gmail.com>
2022-10-12 00:53:12 -0300

    ✨ Register Form Validation

---
+++ web/src/components/Input/index.tsx
@@ -0,0 +1,45 @@
+interface InputProps {
+  label: string
+  name: string
+  type?: string
+  error?: string | boolean | undefined
+  placeholder: string
+  value: string
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  onBlur: (event: React.FocusEvent<HTMLInputElement>) => void
+  required?: boolean
+}
+
+export function Input({
+  name,
+  label,
+  type = 'text',
+  error,
+  placeholder,
+  required,
+  value,
+  onChange,
+  onBlur,
+  ...rest
+}: InputProps) {
+  console.log('🚀 ~ error', error)
+  return (
+    <div className="flex flex-col">
+      <label className="label" htmlFor={name}>
+        {label}
+      </label>
+      <input
+        {...rest}
+        className={`input ${error && 'border border-red-300'}`}
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        required={required}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      <span className="p-2 text-sm text-red-300">{error}</span>
+    </div>
+  )
+}
--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,7 +1,36 @@
+import { Input } from '@/components/Input'
+import { useFormik } from 'formik'
 import { MdArrowBack } from 'react-icons/md'
 import { NavLink } from 'react-router-dom'
+import * as yup from 'yup'
+
+const validationSchema = yup.object().shape({
+  name: yup.string().trim().required('Preencha seu nome.'),
+  username: yup.string().required('Preencha seu nome de usuário.'),
+  email: yup
+    .string()
+    .email('E-mail deve ser válido.')
+    .required('Informe seu e-mail.'),
+  password: yup
+    .string()
+    .min(8, 'Senha deve ter pelo menos 8 caracteres')
+    .required('Digite sua senha.'),
+})
 
 export function Register() {
+  const formik = useFormik({
+    onSubmit: (values) => {
+      console.log(values)
+    },
+    initialValues: {
+      name: '',
+      username: '',
+      email: '',
+      password: '',
+    },
+    validationSchema,
+  })
+
   return (
     <div className="min-h-screen bg-white text-white flex flex-col items-center">
       <header className="w-full flex justify-center px-5 py-5 border-b-2 border-red-300">
@@ -18,60 +47,71 @@ export function Register() {
           <h1 className="text-red-700 font-bold text-xl">Crie sua conta</h1>
         </header>
 
-        <form action="" className="flex flex-col gap-5 my-8">
+        <form
+          onSubmit={formik.handleSubmit}
+          className="flex flex-col gap-5 my-8"
+        >
           <div className="input-container">
-            <label className="label" htmlFor="name">
-              Seu nome
-            </label>
-
-            <input
-              className="input"
+            <Input
+              label="Seu nome"
               name="name"
               type="text"
+              error={formik.touched.name && formik.errors.name}
               placeholder="Digite seu nome"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              required
             />
           </div>
 
           <div className="input-container">
-            <label className="label" htmlFor="username">
-              Seu nome de usuário
-            </label>
-
-            <input
-              className="input"
+            <Input
+              label="Seu nome de usuário"
               name="username"
               type="text"
+              error={formik.touched.username && formik.errors.username}
               placeholder="Digite seu nome de usuário"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              required
             />
           </div>
 
           <div className="input-container">
-            <label className="label" htmlFor="email">
-              Seu e-mail
-            </label>
-
-            <input
-              className="input"
+            <Input
+              label="Seu e-mail"
               name="email"
               type="email"
+              error={formik.touched.email && formik.errors.email}
               placeholder="Digite seu e-mail"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              required
             />
           </div>
 
           <div className="input-container">
-            <label className="label" htmlFor="email">
-              Sua senha
-            </label>
-
-            <input
-              className="input"
+            <Input
+              label="Sua senha"
               name="password"
               type="password"
+              error={formik.touched.password && formik.errors.password}
               placeholder="Digite sua senha"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              required
             />
           </div>
 
-          <button type="submit" className="button button-primary mt-5">
+          <button
+            type="submit"
+            className="button button-primary mt-5 disabled:opacity-80 disabled:cursor-not-allowed"
+            disabled={!formik.isValid}
+          >
             Criar minha conta
           </button>
         </form>
